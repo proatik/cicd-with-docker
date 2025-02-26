e07644d101ac052308498699296daef7f4caf009
Atik Ullah Khan <atikullah.murad@gmail.com>
2025-02-26 12:46:42 +0600

    feat: initialize next.js app with docker and basic components

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+// react components.
+import ClientComponent from "@/components/ClientComponent";
+import ServerComponent from "@/components/ServerComponent";
+import BasicInformation from "@/components/BasicInformation";
+
+const Home = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between py-20 bg-zinc-950">
+      <div className="z-10 w-full max-w-5xl grid grid-cols-2 gap-4 justify-between font-mono">
+        <ClientComponent />
+        <ServerComponent />
+        <BasicInformation />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
