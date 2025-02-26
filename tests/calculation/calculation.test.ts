5afb12521998bcf58b38e10732d7cb4a625edc30
Atik Ullah Khan <atikullah.murad@gmail.com>
2025-02-26 15:50:59 +0600

    fix: add wrong test case for demo

--- tests/calculation/calculation.test.ts
+++ tests/calculation/calculation.test.ts
@@ -2,7 +2,7 @@ import { add, subtract } from "@/lib/calculation";
 
 describe("Calculation functions", () => {
   test("add should correctly add two numbers", () => {
-    expect(add(1, 2)).toBe(3);
+    expect(add(1, 2)).toBe(4);
     expect(add(-1, -1)).toBe(-2);
     expect(add(0, 0)).toBe(0);
   });
